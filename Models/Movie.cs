94ca0dd9e2b040b52b7880e3d2023112fd055856
Baptiste Pesquet <bpesquet@gmail.com>
2022-10-09 16:26:08 +0200

    Add Movie model class

---
+++ Models/Movie.cs
@@ -0,0 +1,12 @@
+namespace MvcMovie.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public DateTime ReleaseDate { get; set; }
+
+    public string Genre { get; set; } = null!;
+}
