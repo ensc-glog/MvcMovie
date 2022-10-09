fa9fcee86a960d6f23f400d8b4987c06345897d3
Baptiste Pesquet <bpesquet@gmail.com>
2022-10-09 16:26:21 +0200

    First migration

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data;
+
+public class MvcMovieContext : DbContext
+{
+    public DbSet<Movie> Movies { get; set; } = null!;
+    public string DbPath { get; private set; }
+
+    public MvcMovieContext()
+    {
+        // Path to SQLite database file
+        DbPath = "MvcBlog.db";
+    }
+
+    // The following configures EF to create a SQLite database file locally
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+        // Use SQLite as database
+        options.UseSqlite($"Data Source={DbPath}");
+        // Optional: log SQL queries to console
+        options.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information);
+    }
+}
