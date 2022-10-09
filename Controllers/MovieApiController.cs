d0cda8376745d091ea3aee27e6dbd86c818a0699
Baptiste Pesquet <bpesquet@gmail.com>
2022-10-09 18:45:00 +0200

    Add CRUD operations to movie api controller

--- Controllers/MovieApiController.cs
+++ Controllers/MovieApiController.cs
@@ -21,4 +21,72 @@ public class MovieApiController : ControllerBase
     {
         return await _context.Movies.ToListAsync();
     }
+
+    // GET: api/MovieApi/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Movie>> GetMovie(int id)
+    {
+        var movie = await _context.Movies.FindAsync(id);
+
+        if (movie == null)
+            return NotFound();
+
+        return movie;
+    }
+
+    // POST: api/MovieApi
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<Movie>> PostMovie(Movie movie)
+    {
+        _context.Movies.Add(movie);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetTodo", new { id = movie.Id }, movie);
+    }
+
+    // PUT: api/MovieApi/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutMovie(int id, Movie movie)
+    {
+        if (id != movie.Id)
+            return BadRequest();
+
+        _context.Entry(movie).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!MovieExists(id))
+                return NotFound();
+            else
+                throw;
+        }
+
+        return NoContent();
+    }
+
+    // DELETE: api/MovieApi/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteMovie(int id)
+    {
+        var movie = await _context.Movies.FindAsync(id);
+        if (movie == null)
+            return NotFound();
+
+        _context.Movies.Remove(movie);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    // Returns true if a movie with specified id already exists
+    private bool MovieExists(int id)
+    {
+        return _context.Movies.Any(m => m.Id == id);
+    }
 }
