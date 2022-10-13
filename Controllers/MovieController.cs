c4f3f50b6d7b2c93afa8f05c6d7796f6be3cf711
Baptiste Pesquet <bpesquet@gmail.com>
2022-10-13 13:45:08 +0200

    Add movie details action and view

--- Controllers/MovieController.cs
+++ Controllers/MovieController.cs
@@ -13,10 +13,29 @@ public class MovieController : Controller
         _context = context;
     }
 
+    // GET: Movie
     public async Task<IActionResult> Index()
     {
         var movies = await _context.Movies.OrderBy(m => m.Title).ToListAsync();
 
         return View(movies);
+    }
+
+    // GET: Movies/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+
+        var movie = await _context.Movies
+            .FirstOrDefaultAsync(m => m.Id == id);
+        if (movie == null)
+        {
+            return NotFound();
+        }
+
+        return View(movie);
     }
 }
