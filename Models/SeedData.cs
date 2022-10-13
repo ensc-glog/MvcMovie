23beff37ec99805966f18f4c5d6405d07648f1cf
Baptiste Pesquet <bpesquet@gmail.com>
2022-10-13 13:44:18 +0200

    Code reformat

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -16,34 +16,33 @@ public class SeedData
 
             // Add several movies
             context.Movies.AddRange(
-                                new Movie
-                                {
-                                    Title = "When Harry Met Sally",
-                                    ReleaseDate = DateTime.Parse("1989-2-12"),
-                                    Genre = "Romantic Comedy",
-                                },
+                new Movie
+                {
+                    Title = "When Harry Met Sally",
+                    ReleaseDate = DateTime.Parse("1989-2-12"),
+                    Genre = "Romantic Comedy",
+                },
 
-                                new Movie
-                                {
-                                    Title = "Ghostbusters ",
-                                    ReleaseDate = DateTime.Parse("1984-3-13"),
-                                    Genre = "Comedy",
-                                },
+                new Movie
+                {
+                    Title = "Ghostbusters ",
+                    ReleaseDate = DateTime.Parse("1984-3-13"),
+                    Genre = "Comedy",
+                },
 
-                                new Movie
-                                {
-                                    Title = "Ghostbusters 2",
-                                    ReleaseDate = DateTime.Parse("1986-2-23"),
-                                    Genre = "Comedy",
-                                },
-
-                                new Movie
-                                {
-                                    Title = "Rio Bravo",
-                                    ReleaseDate = DateTime.Parse("1959-4-15"),
-                                    Genre = "Western"
-                                }
-                            );
+                new Movie
+                {
+                    Title = "Ghostbusters 2",
+                    ReleaseDate = DateTime.Parse("1986-2-23"),
+                    Genre = "Comedy",
+                },
+                new Movie
+                {
+                    Title = "Rio Bravo",
+                    ReleaseDate = DateTime.Parse("1959-4-15"),
+                    Genre = "Western"
+                }
+            );
 
             // Commit changes into DB
             context.SaveChanges();
